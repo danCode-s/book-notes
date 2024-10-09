800ef199975c8acf86de6a9d0653ce55634802ec
danCode-s <dancodes9@gmail.com>
2024-10-09 19:41:21 +0500

    get data from database and show it

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ const port = 3001;
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(express.static("public"));
 
+
+//sample data
+const books = [
+    {id: 1, book_name: "Harry Potter and the Philosopher's Stone", author: "j.k rowling", ISBN: 9780747532699, review: "GREAT BOOK", rating: 5},
+    {id: 2, book_name: "Harry Potter and the Philosopher's Stone", author: "j.k rowling", ISBN: 9780747532699, review: "GOOD BOOK", rating: 4},
+];
+
+
 // Connect to the database
 const db = new pg.Client({
     user: "postgres",
@@ -29,19 +37,23 @@ async function getBookImageURL(ISBN){
     const response = await axios.get(`https://covers.openlibrary.org/b/ISBN/${ISBN}-L.jpg`);
     return response.config.url;
 }
+// get data from the database
+async function checkCurrentBooks(){
+    const result = await db.query("SELECT b.id, b.name, b.author, b.isbn, r.review, r.rating, b.img_url FROM book_names b JOIN review r ON b.id = r.book_id");
+    const books = result.rows;
+    return books;
+}
 
-//sample data
-const books = [
-    {id: 1, book_name: "Harry Potter and the Philosopher's Stone", author: "j.k rowling", ISBN: 9780747532699, review: "GREAT BOOK", rating: 5},
-    {id: 2, book_name: "Harry Potter and the Philosopher's Stone", author: "j.k rowling", ISBN: 9780747532699, review: "GOOD BOOK", rating: 4},
-];
+
 
 books.forEach(async book => {
     book['img_url'] = await getBookImageURL(book.ISBN);
 })
 
+
 app.get("/", async (req, res) => {
-    console.log(books);
+    const books = await checkCurrentBooks();
+    console.log(books)
     res.render("index.ejs", {books: books});
 
 })
