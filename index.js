dd1015a1359dfe8a2c54fecf548c3a8dc443de83
danCode-s <dancodes9@gmail.com>
2024-10-09 15:24:26 +0500

    get image urls from the api, save locally and show

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 import pg from "pg";
@@ -10,13 +10,26 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(express.static("public"));
 
+
+
+async function getBookImageURL(ISBN){
+    const response = await axios.get(`https://covers.openlibrary.org/b/ISBN/${ISBN}-L.jpg`);
+    return response.config.url;
+}
+
 const books = [
     {id: 1, book_name: "Harry Potter and the Philosopher's Stone", author: "j.k rowling", ISBN: 9780747532699, review: "GREAT BOOK", rating: 5},
     {id: 2, book_name: "Harry Potter and the Philosopher's Stone", author: "j.k rowling", ISBN: 9780747532699, review: "GOOD BOOK", rating: 4},
 ];
 
-app.get("/", (req, res) => {
+books.forEach(async book => {
+    book['img_url'] = await getBookImageURL(book.ISBN);
+})
+
+app.get("/", async (req, res) => {
+    console.log(books);
     res.render("index.ejs", {books: books});
+
 })
 
 
