169f1c75ae6a4d559a997f473eddaf29ab2f9fd5
danCode-s <dancodes9@gmail.com>
2024-10-09 19:30:37 +0500

    basic comments and database connecet

--- index.js
+++ index.js
@@ -5,18 +5,32 @@ import pg from "pg";
 
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
+
+
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(express.static("public"));
 
+// Connect to the database
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "book",
+    password: "your-db-password",
+    port: 5432
+})
+db.connect();
 
 
+// get url of the image using open library api
 async function getBookImageURL(ISBN){
     const response = await axios.get(`https://covers.openlibrary.org/b/ISBN/${ISBN}-L.jpg`);
     return response.config.url;
 }
 
+//sample data
 const books = [
     {id: 1, book_name: "Harry Potter and the Philosopher's Stone", author: "j.k rowling", ISBN: 9780747532699, review: "GREAT BOOK", rating: 5},
     {id: 2, book_name: "Harry Potter and the Philosopher's Stone", author: "j.k rowling", ISBN: 9780747532699, review: "GOOD BOOK", rating: 4},
