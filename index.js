fbf82ebb3adee88191aeca142cccd8846882a020
danCode-s <dancodes9@gmail.com>
2024-10-09 14:52:49 +0500

    sample data

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(express.static("public"));
 
+const books = [
+    {id: 1, book_name: "Harry Potter and the Philosopher's Stone", author: "j.k rowling", ISBN: 9780747532699, review: "GREAT BOOK", rating: 5},
+    {id: 2, book_name: "Harry Potter and the Philosopher's Stone", author: "j.k rowling", ISBN: 9780747532699, review: "GOOD BOOK", rating: 4},
+];
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
